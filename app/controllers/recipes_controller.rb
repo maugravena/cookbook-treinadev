e6e64f4fca99cb3075f4b853025449f28dab31c3
Mauricio <maugravena@gmail.com>
2019-08-20 21:40:09 -0300

    create action and recipe_params method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,4 +10,15 @@ class RecipesController < ApplicationController
   def new
     @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
+  end
 end
