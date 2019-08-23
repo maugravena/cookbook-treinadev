2b63bc4b1b31af7e07dfd5bde09be5d5dadfb9f1
Mauricio <maugravena@gmail.com>
2019-08-23 19:31:00 -0300

    format params method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,6 +29,14 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
+    params
+      .require(:recipe)
+      .permit(:title,
+              :recipe_type,
+              :cuisine,
+              :difficulty,
+              :cook_time,
+              :ingredients,
+              :cook_method)
   end
 end
