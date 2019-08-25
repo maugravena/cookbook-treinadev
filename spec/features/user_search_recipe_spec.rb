5cda01619efb8d019e700daa09b42580908a33ac
Mauricio <maugravena@gmail.com>
2019-08-25 12:40:44 -0300

    spec for search recipe

---
+++ spec/features/user_search_recipe_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+feature 'User search recipe' do
+  scenario 'successfully' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    RecipeType.create(name: 'Entrada')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    Cuisine.create(name: 'Arabe')
+    Recipe.create(title: 'Bolo de cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    visit root_path
+    fill_in 'Pesquisar receita', with: 'Bolo de cenoura'
+    click_on 'Pesquisar'
+
+    expect(page).to have_css('h1', text: 'Bolo de cenoura')
+  end
+
+  scenario 'search by exact name and not find' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    RecipeType.create(name: 'Entrada')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    Cuisine.create(name: 'Arabe')
+    Recipe.create(title: 'Torta de maça', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    visit root_path
+    fill_in 'Pesquisar receita', with: 'Bolo de cenoura'
+    click_on 'Pesquisar'
+
+    expect(page).to have_content 'Não foi possível encontrar a receita'
+  end
+
+  scenario 'search for a recipe by partial name and find more than one' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    RecipeType.create(name: 'Entrada')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    Cuisine.create(name: 'Arabe')
+    recipe = Recipe.create(title: 'Torta de maça', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    RecipeType.create(name: 'Entrada')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    Cuisine.create(name: 'Arabe')
+    other_recipe = Recipe.create(title: 'Torta de morango', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+    visit root_path
+    fill_in 'Pesquisar receita', with: 'tor'
+
+    expect(page).to have_css('h1', text: recipe.title)
+    expect(page).to have_css('h1', text: other_recipe.title)
+  end
+end
