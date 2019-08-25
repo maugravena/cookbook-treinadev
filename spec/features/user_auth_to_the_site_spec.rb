17f647a5bbf1d61f22c81d04e7c4d60aa725dce3
Mauricio <maugravena@gmail.com>
2019-08-25 15:19:20 -0300

    user logout scenario

--- spec/features/user_auth_to_the_site_spec.rb
+++ spec/features/user_auth_to_the_site_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'User login to the site' do
+feature 'User authenticates to the site' do
   scenario 'successfully' do
     user = User.create!(email: 'user@amail.com', password: '123456')
 
@@ -16,5 +16,24 @@ feature 'User login to the site' do
     expect(current_path).to eq root_path
     expect(page).not_to have_link 'Entrar'
     expect(page).to have_link 'Sair'
+  end
+
+  scenario 'user logout to the site' do
+    user = User.create!(email: 'user@amail.com', password: '123456')
+
+    visit root_path
+    click_on 'Entrar'
+
+    within('form') do
+      fill_in 'Email', with: user.email
+      fill_in 'Senha', with: user.password
+      click_on 'Entrar'
+    end
+
+    click_on 'Sair'
+
+    expect(current_path).to eq root_path
+    expect(page).not_to have_link 'Sair'
+    expect(page).to have_link 'Entrar'
   end
 end
